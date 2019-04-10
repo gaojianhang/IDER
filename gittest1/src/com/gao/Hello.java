e67e141c140c72074c1d57cbdca311c22fb9a744
gaojianhang <952859494@qq.com>
2019-04-10 11:36:07 +0800

    提交gittest1

---
+++ gittest1/src/com/gao/Hello.java
@@ -0,0 +1,12 @@
+package com.gao;
+
+/**
+ * @author gaojianhang
+ * @date 2019/4/10 0010 - 11:19
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好世界");
+    }
+
+}
